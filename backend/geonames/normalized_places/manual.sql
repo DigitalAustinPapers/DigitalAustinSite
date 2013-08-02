c488444864690b9cdaf3c02020f910df09fa3e65
Ben W. Brumfield <benwbrum@gmail.com>
2013-08-02 11:36:27 -0500

    sorted manual place inserts

--- backend/geonames/normalized_places/manual.sql
+++ backend/geonames/normalized_places/manual.sql
@@ -1,22 +1,14 @@
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Devonshire, England", 50.75, -3.75); -- Devonshire, England.xml
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("London, England", 51.50853, -0.12574); -- London, England.xml
+
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Austin Colony", 29.794444,  -96.104722); -- Austin Colony
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Austinville, VA", 36.85123, -80.91202);
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Burton Mine, MO", 37.976944, -90.048611); -- Burton Mine, MO
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Burton Mine, St Genevieve, LA", 37.976944, -90.048611); -- Burton Mine, St Genevieve, LA
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Leona Vicario, Coahuila", 25.41942, -101.00128); -- Leona Vicario, Coahuila
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Little Rock, AR", 34.736111, -92.331111); -- Little Rock, AR
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Mission San Juan, Texas", 29.332687,  -98.455289); -- Mission San Juan, Texas
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Oak Grove, TX", 32.540147, -96.317266); -- Oak Grove, Texas
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Potosi, Washington County, MO", 37.937881,  -90.781932); -- Potosi, Washington County, MO
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("St. Martinville, LA", 30.125053, -91.830593); -- San Martinville, Attakapas, Louisiana
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Washington City, U.S.", 38.895111, -77.036667); -- Washington City, U.S.
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Washington County, MO", 37.937881,  -90.781932); -- Washington County, MO
+INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Camargo, Chihuahua", 27.67842, -105.17136); -- Comargo,Chihuahua
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Cerralvo, Nuevo León", 26.08548, -99.61631); -- Cerralvo, Nuevo León
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Charleston, SC", 32.77657, ); -- Charleston, South Carolian
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("China Grove, TX", 29.38885, -98.3489); -- China Grove, Texas
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Camargo, Chihuahua", 27.67842, -105.17136); -- Comargo,Chihuahua
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Damascus, Henry County, OH",41.41394, -84.01856); -- Demascus, Henry County, Ohio
+INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Devonshire, England", 50.75, -3.75); -- Devonshire, England.xml
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Dover, NH", 43.19786, -70.87367); -- Dover, New Hampshire
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Eatonton, Putnam County, GA", 33.3268, -83.3885); -- Eatenton, Putnam County, Georgia
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Elk Hill, VA", 37.71765, -78.0875); -- Elk-Hill, Virginia
@@ -29,8 +21,9 @@ INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Hempstead County, AR", 33.
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Henderson County, Tennessee", 35.65426, -88.38799); -- Henderson County, Tennessee
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Iberville Parish, LA", 30.2585, -91.34936); --  Ibirville Parish, LA
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Jessamine County, KY", 37.87203, -84.58093); -- Jessamine County, Kentucky
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Leona Vicario, Coahuila", 30.51478, -107.93604); -- Leona Vicario, Coahuila
+INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Leona Vicario, Coahuila", 25.41942, -101.00128); -- Leona Vicario, Coahuila
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Little Rock, AR", 34.74648, -92.28959); -- Little Rock, AR
+INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("London, England", 51.50853, -0.12574); -- London, England.xml
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Long Prairie, AR", 33.42678, -91.76402); -- Long Prairie, AK
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Lynch''s Ferry, TX",  29.7493, -95.0814); -- Lynchs Ferry, Texas
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Matamoros", 25.88333, -97.5); -- Matamoras, Texxas
@@ -38,21 +31,22 @@ INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Mercer County, KY", 37.811
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Mill Creek, TX", 30.15549, -96.4933); -- Mill Creek, Texas.
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Miller County, AR", 33.31215, -93.89156); -- Miller County, Arkansas Territory
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Mine a Breton, MO", 38.02894, -90.85347); -- Mine a Breton, Missouri
+INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Mission San Juan, Texas", 29.332687,  -98.455289); -- Mission San Juan, Texas
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Monclova, Coahuila", 26.90687, -101.42056); -- Moncolva, Coahuila
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Monroe County, MS", 33.89224, -88.48047); -- Monroe County, Mississippi
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Monterrey, Nuevo Leon", 25.67506, -100.31846); -- Monterrey, Nuevo Leon
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Montgomery County, MO", 38.94149, -91.47021); -- Montgomery County, MO
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Mount Carmel, MO", 39.16754, -91.48877); -- Mount Carmel, Missouri
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Murfreesboro, TN", 35.84562, -86.39027); -- Murfresborough, TN
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Russellville, KY", 36.84532, -86.88722); -- Near Russellville, Logan Country, Kentucky
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("New London, MO", 39.58532, -91.40098); -- New London, Missouri
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("New Madrid, MO", 36.58645, -89.52785); -- New Madrid, Missouri
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("New York, NY", 40.71427, -74.00597); -- New York, New York.
+INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Oak Grove, TX", 32.540147, -96.317266); -- Oak Grove, Texas
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Opelousas, LA", 30.53353, -92.08151); -- Opeolusas, LA
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Peach Creek, TX", 30.53854, -96.19468); -- Peach Creek, TX
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Point Pleasant, TX", 32.48597, -97.19751); -- Point Pleasant, Texas
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Veracruz, Mexico", 19.18074, -96.13405); -- Port of Veracruz, MX
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Potosi, MO", 37.93644, -90.78791); -- Potosi, Mossouri
+INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Potosi, Washington County, MO", 37.937881,  -90.781932); -- Potosi, Washington County, MO
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Rapides Parish, LA", 31.19862, -92.5332); -- Rapides Parish, Louisiana
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Ripley, OH", 38.74563, -83.84492); -- Ripley, Brown County, Ohio
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Round Lick, TN", 36.26366, -86.06943); -- Round Lick, Tennessee
@@ -72,10 +66,12 @@ INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("St. Albans, VT", 44.81088,
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("St. Charles, MO", 38.78394, -90.48123); -- St. Charles, MO
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("St. Genevieve, MO", 37.98144, -90.04178); -- St. Genevieve, MO
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("St. Louis, MO", 38.62727, -90.19789); -- St. Louis, MO
+INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("St. Martinville, LA", 30.125053, -91.830593); -- San Martinville, Attakapas, Louisiana
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Tuscumbia, Alabama", 34.7312, -87.70253); -- Tuscumbia, Franklin County, North Alabama
+INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Veracruz, Mexico", 19.18074, -96.13405); -- Port of Veracruz, MX
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Waco Village, TX", 31.551389, -97.155833); -- Waco Village, TX
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Washington City, MO", 38.55165, -91.01492); -- Washington City, MO
-INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Washington City, U.S.", 38.89511, -77.03637); -- Washington City, U.S.
+INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Washington City, U.S.", 38.895111, -77.036667); -- Washington City, U.S.
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Washington County, MO", 37.96168, -90.87742); -- Washington County, MO
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Washington County, TX", 30.21453, -96.40344); -- Washington County,Texas
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Washington, Hempstead County, AR", 33.774, -93.68268); -- Washington, Hempstead County, Arkansas Territory
@@ -84,4 +80,3 @@ INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Wilkinson County, MS", 31.
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Woodville, MS", 31.10462, -91.29956); -- Woodville, Miss
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Wythe County, VA", 36.91713, -81.07859); -- Wythe County, VA
 INSERT INTO NormalizedPlace (name, lat, lng) VALUES ("Zanesville, OH", 39.94035, -82.01319); -- Zainsville, OH
-
