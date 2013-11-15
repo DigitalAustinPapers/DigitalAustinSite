82c6e16b9ac94f613ac0cb8220ffcbc1d4ff0920
Stephen Eisenhauer <bhs2007@gmail.com>
2013-11-15 14:10:11 -0600

    Fix broken path to jquery script source on results.php

--- results.php
+++ results.php
@@ -26,7 +26,7 @@
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 
 <!-- JQuery and JQuery UI -->
-<script src="js/jquery/jquery-1.9.1.min.js"></script>
+<script src="js/jquery-1.9.1.min.js"></script>
 <script src="js/jquery-ui-1.10.3.min.js"></script>
 
 <!-- Maps API 
