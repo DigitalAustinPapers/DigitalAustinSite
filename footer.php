80557954efc14d800931793d255f469b0a993ba0
jason-ellis <jason.c.ellis@gmail.com>
2015-06-23 11:06:23 -0500

    move jquery and jquery-ui to src folder and update footer

--- footer.php
+++ footer.php
@@ -13,12 +13,9 @@
 
 </div><!-- wrapper -->
 
-<!--TODO: Cleanup scripts below-->
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="js/jquery-ui-1.10.3.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
+<script src="src/jquery/jquery-1.9.1.min.js"></script>
+<script src="src/jquery-ui/jquery-ui-1.10.3.min.js"></script>
+<script src="src/bootstrap/dist/js/bootstrap.min.js"></script>
 
 </body>
 </html>
