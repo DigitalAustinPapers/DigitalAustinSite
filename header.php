552f3594908575940a5b79eb8a9467e06718a87a
jason-ellis <jason.c.ellis@gmail.com>
2015-06-23 12:13:55 -0500

    Added local js resources. Updated head and footer to use local resources. Rough responsive navbar.

--- header.php
+++ header.php
@@ -4,49 +4,46 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
   <title>Stephen F. Austin - Digital Collection</title>
 
-  <!-- Bootstrap -->
-<!--  <link href="css/bootstrap.min.css" rel="stylesheet">-->
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/style.css">
 
+  <!-- scripts -->
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <script src="src/js/html5shiv/html5shiv-3.7.2.min.js"></script>
+  <script src="src/js/respond/respond-1.4.2.min.js"></script>
   <![endif]-->
 
-  <!-- Bootstrap -->
-
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="style.css" /> <!-- Merged CSS -kmd -->
-  <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
-<div id = "wrapper" class="shadow">
+<div id="main-wrapper">
 
-<header id="header" class="header">
-  <div id = "home">
-    <h1><a href="index.php"><span class="digital">Digital</span> <span class="austin">Austin</span> <span class="papers">Papers</span></a></h1>
-  </div>
-  <div id = "menu">
-    <ul id="nav">
-      <li>
-      <!-- Changed navigation -kmd -->
-        <a href="about.php">About</a>
-      </li>
-      <li>
-      <!-- Link to generic search page, other searches linked from there as needed -kmd -->
-        <a href="advanced.php">Search</a>
-      </li>
-      <li>
-        <a href="browse.php">Browse</a>
-      </li>
-      <li>
-        <a href="contact.php">Contact</a>
-      </li>
-    </ul>
-  </div>
-</header><!-- header -->
+<nav id="main-nav" role="navigation" class="navbar navbar-default hidden-print">
+  <div class="container">
+
+    <div class="navbar-header">
+      <div class="navbar-text navbar-brand visible-xs-block">
+        <a id="site-name-mobile" class="btn btn-link" href="index.php" title="Home">Digital Austin Papers</a>
+      </div><!-- navbar-text -->
+      <button id="main-nav-toggle" class="navbar-toggle btn btn-success btn-sm navbar-left collapsed" type="button" data-toggle="collapse" data-target="#primary-site-menu" aria-expanded="false">MENU</button>
+    </div><!-- navbar-header -->
+
+    <div class="navbar-collapse collapse" id="primary-site-menu" role="navigation" aria-expanded="false">
+      <div class="navbar-left hidden-xs">
+        <a id="navbar-link-brand" class="navbar-brand" href="index.php">Digital Austin Papers</a>
+      </div><!-- navbar-left -->
+      <div class="navbar-right">
+        <ul class="menu nav navbar-nav">
+          <li><a id="navbar-link-about" href="about.php">About</a></li>
+          <li><a id="navbar-link-search" href="advanced.php">Search</a></li>
+          <li><a id="navbar-link-browse" href="browse.php">Browse</a></li>
+          <li><a id="navbar-link-contact" href="contact.php">Contact</a></li>
+        </ul>
+      </div><!-- navbar-right -->
+
+    </div><!-- container -->
+</nav>
