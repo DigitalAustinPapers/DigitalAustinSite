624a9884d8adb39d72f4a8eb71d34d1c3d1f5169
jason-ellis <jason.c.ellis@gmail.com>
2015-06-24 13:34:03 -0500

    initial contact form template and render

--- contact.php
+++ contact.php
@@ -1,30 +1,8 @@
-<?php include('header.php'); ?>
+<?php require_once 'TemplateRenderer.class.php';
 
-			<div id = "form">
-				<h2>Contact Form</h2> 
-			<p>Please fill out the form below to send email to the Digital Austin Collection team <em>(all fields are required)</em>:</p> 
-			<form action="send.php" method="post"> 
-				<p>Your Name:<br /> 
-				<input type="text" name="contact_name" size="30" maxlength="100" tabindex="1" /> 
-				</p> 
-				<p>Your Email Address:<br /> 
-				<input type="text" name="contact_email" size="30" maxlength="100" tabindex="2" /> 
-				</p> 
-				<p>Reason for Email:
-				<select name="contact_reason" size="1" tabindex="3"> 
-					<option label="Question" value="question" selected="selected">Question</option> 
-					<option label="Comment" value="comment">Comment</option> 
-					<option label="Suggestion" value="suggestion">Suggestion</option> 
-					<option label="Problem" value="problem">Problem</option> 
-				</select> 
-				</p> 
-				<p>Your Message:<br /> 
-				<textarea name="contact_message" rows="8" cols="40" tabindex="4"></textarea> 
-				</p>
-				<p> 
-				<input type="submit" name="Submit" value="Send!" tabindex="5" /> 
-				</p> 
-			</form> 
-			</div>
+// Include any logic for this page below
 
-<?php include('footer.php'); ?>
+
+$renderer = new TemplateRenderer();
+// Include any variables as an array in the second param
+print $renderer->render('contact.html.twig');
