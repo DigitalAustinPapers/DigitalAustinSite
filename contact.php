f3358d66d7bb01057acc0fee98507c50e4119660
jason-ellis <jason.c.ellis@gmail.com>
2015-06-25 15:01:06 -0500

    back to previous method of using variables.

--- contact.php
+++ contact.php
@@ -1,11 +1,8 @@
-<?php
-require_once 'src/TemplateRenderer.class.php';
-$template = new TemplateRenderer();
+<?php require_once 'src/TemplateRenderer.class.php';
 
 // Include any logic for this page below
 
 
-
-// Variables should be passed to the template with $template->var = vars
-// before rendering
-print $template->render('contact.html.twig');
+$renderer = new TemplateRenderer();
+// Include any variables as an array in the second param
+print $renderer->render('contact.html.twig');
