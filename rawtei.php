7e244963fcfe0dc5a39d4772236380e2dcf5aa67
jason-ellis <jason.c.ellis@gmail.com>
2015-06-30 13:09:45 -0500

    remove include and add simplified queryDB function.

--- rawtei.php
+++ rawtei.php
@@ -8,14 +8,24 @@ header('Content-type: text/xml');
   print '<?xml-stylesheet type="text/xsl" href="xsl/teibp.xsl"?>';
   print "\n";
 
-?>
 
-<?php include('php/database.php'); ?>
-<?php include('php/document.php'); ?>
+include('php/database.php');
 
+function queryDB(){
+  $id = $_GET['id'];
+  $new_id = str_replace(".xml", "", $id);
 
-<?php
-	connectToDB();
-	$result = queryDB();
-	print str_replace('<?xml version="1.0" encoding="Windows-1252"?>', '', $result['xml']);
-?>
+  $query =
+    "SELECT d.id id,
+        d.xml xml
+     FROM Document d
+     WHERE d.id = '$new_id'";
+
+  $result = mysqli_query($GLOBALS["___mysqli_ston"], $query) or die(((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));;
+
+  return mysqli_fetch_assoc($result);
+}
+
+connectToDB();
+$result = queryDB();
+print str_replace('<?xml version="1.0" encoding="Windows-1252"?>', '', $result['xml']);
