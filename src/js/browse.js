9f47c7f281dcbb0b491782e4e55fbc54a17ee060
jason-ellis <jason.c.ellis@gmail.com>
2015-07-06 15:58:07 -0500

    pagination for browsing

--- src/js/browse.js
+++ src/js/browse.js
@@ -1,34 +1,54 @@
+/*
+ * Toggle collapsing elements in results
+ */
+
+function expandAll(button) {
+    button.html('Collapse all').attr('data-state', 'expanded');
+    $('#browse-results .collapse').each(function() {
+        $(this).collapse('show');
+    });
+}
+
+function collapseAll(button) {
+    button.html('Expand all').attr('data-state', 'collapsed');
+    $('#browse-results .collapse').each(function() {
+        $(this).collapse('hide');
+    });
+}
+
 // Expand and collapse all nodes and update expand/collapse button
 $('#collapse-expand-all').on('click', function(e) {
-    if($(this).attr('data-state') == 'expand') {
-        $(this).html('Collapse all').attr('data-state', 'collapse');
-        $('.collapse').each(function() {
-            $(this).collapse('show');
-        });
-    } else if($(this).attr('data-state') == 'collapse') {
-        $(this).html('Expand all').attr('data-state', 'expand');
-        $('.collapse').each(function() {
-            $(this).collapse('hide');
-        });
+    if($(this).attr('data-state') == 'collapsed') {
+        expandAll($(this));
+    } else if($(this).attr('data-state') == 'expanded') {
+        collapseAll($(this));
     }
 });
 
-var categorizedResults = $('#browse-results').find('li');
-console.log('categorizedResults = '+categorizedResults);
+// Selector for list to be populated with paginated results
+var listId = $('#browse-results');
+// Selector for list content to be paginated
+var categorizedResults = $('#browse-results > li');
 
-$(".pagination").paging(1337, { // make 1337 elements navigatable
-    format: '[< ncnnn! >]', // define how the navigation should look like and in which order onFormat() get's called
+$(".pagination").paging(totalHeadings, { // make 1337 elements navigatable
+    format: '[< nncnn! >]', // define how the navigation should look like and in which order onFormat() get's called
     perpage: 10, // show 10 elements per page
     lapping: 0, // don't overlap pages for the moment
     page: 1, // start at page, can also be "null" or negative
     onSelect: function (page) {
-        updatePage(page);
-        console.log('this inside paging = '+this);
+        updatePage(listId, categorizedResults, this.slice);
+        console.log(this);
     },
     onFormat: function (type) {
         switch (type) {
             case 'block': // n and c
-                return '<li><a href="#">' + this.value + '</a></li>';
+                if (this.page === this.value)
+                    return '<li class="active">' +
+                        '<a href="#">' + this.value + '</a>' +
+                        '</li>';
+                return '<li>' +
+                '<a href="#">' + this.value + '</a>' +
+                '</li>';
             case 'next': // >
                 return '<li><a href="#" aria-label="Next">' +
                     '<span aria-hidden="true">&raquo;</span>' +
@@ -45,9 +65,15 @@ $(".pagination").paging(1337, { // make 1337 elements navigatable
     }
 });
 
-function updatePage(newPage) {
-    var resultsList = $('#browse-results');
-    resultsList.empty();
-    resultsList.append(categorizedResults[0]);
-    console.log('newPage = '+newPage);
+function updatePage(listId, pagedElements, pageSlice) {
+    /* Updates the current page when a page button is clicked
+     * @param {object}
+     * @param {array} pageSlice This is an array with 2 values:
+     *     The start and end values to slice the page
+     */
+    var newPage = pagedElements.slice(pageSlice[0], pageSlice[1]);
+    collapseAll($('#collapse-expand-all'));
+
+    listId.empty();
+    listId.append(newPage);
 }
