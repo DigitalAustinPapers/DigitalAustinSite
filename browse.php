af069240fe0ed55d54f447bf9bbd6c28611ca92c
jason-ellis <jason.c.ellis@gmail.com>
2015-08-14 14:40:33 -0500

    include creation date in browse query and display it seperately in browse results list.

--- browse.php
+++ browse.php
@@ -5,7 +5,7 @@ $connection = connectToDB();
 
 function getNormalSql($normalTable, $joinColumn) {
 	return		"SELECT np.name heading, 
-						d.id id,  
+						d.id id,
 						if(RIGHT(np.name, 
 								LOCATE(' ', REVERSE(np.name)) - 1)
 							<>'',
@@ -13,7 +13,8 @@ function getNormalSql($normalTable, $joinColumn) {
 								LOCATE(' ', REVERSE(np.name)) - 1) , 
 								np.name) surname,
 						d.title title,
-						d.summary summary
+						d.summary summary,
+						d.creation creation
 					FROM {$normalTable} np
 					INNER JOIN Document d 
 					ON np.id=d.{$joinColumn} 
@@ -22,7 +23,8 @@ function getNormalSql($normalTable, $joinColumn) {
 					d.id id,
 					'zzzzz' surname,
 					d.title title,
-					d.summary summary
+					d.summary summary,
+					d.creation creation
 					from Document d
 					where d.{$joinColumn} is null
 					ORDER BY surname, heading";
@@ -34,7 +36,8 @@ function getNormalPlaceSql($joinColumn) {
 					d.id id,
 					d.title title,
 					d.summary summary,
-					np.name order_key 
+					np.name order_key,
+					d.creation creation
 				FROM NormalizedPlace np
 				INNER JOIN Document d 
 				ON np.id=d.sentFromPlace
@@ -43,7 +46,8 @@ function getNormalPlaceSql($joinColumn) {
 					d.id id,
 					d.title title,
 					d.summary summary,
-					'zzzzzzzzzz' order_key
+					'zzzzzzzzzz' order_key,
+					d.creation creation
 				FROM Document d
 				WHERE d.{$joinColumn} is null 
 				ORDER BY order_key, title";
@@ -53,9 +57,9 @@ function getNormalPlaceSql($joinColumn) {
 function getResult($targetBrowseBy) {
 	$sqlStatements = 
 		array('date' => 
-				"SELECT left(creation,4) heading, id, title, summary from Document order by creation",
+				"SELECT left(creation,4) heading, creation, id, title, summary from Document order by creation",
 			'page' =>
-				"SELECT concat('Page ', trim(leading '0' from right(id,4))) heading, id, title, summary from Document order by id",
+				"SELECT concat('Page ', trim(leading '0' from right(id,4))) heading, creation, id, title, summary from Document order by id",
 			'author' =>
 				getNormalSql('NormalizedPerson', 'sentFromPerson'),
 			'recipient' =>
@@ -90,6 +94,7 @@ while ($row = mysqli_fetch_array($result))
   $id = $row['id'];
   $title = $row['title'];
   $summary = $row['summary'];
+	$date = $row['creation'];
 
   if($heading != $oldHeading) {
     # print the header
@@ -101,7 +106,8 @@ while ($row = mysqli_fetch_array($result))
   }
   $results_list[$cleanHeading][$id] = array(
     'document_title' => $title,
-    'document_summary' => $summary,);
+    'document_summary' => $summary,
+		'document_date' => $date);
 
   $oldHeading=$heading;
 }
