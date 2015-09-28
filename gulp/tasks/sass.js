3d310cc164604b78bdbd27ce3367d92191c478d1
jason-ellis <jason.c.ellis@gmail.com>
2015-09-28 14:03:12 -0500

    added stream-combiner2 to catch errors in gulp and output to console isntead of throwing

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,18 +1,28 @@
 'use strict';
 
+// Using stream-combiner2 to catch errors per gulp at:
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/combining-streams-to-handle-errors.md#combining-streams-to-handle-errors
+
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
+    combiner = require('stream-combiner2'),
     config = require('../config').sass;
 
 
 gulp.task('sass', function() {
-  gulp.src(config.src)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-      .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(config.dest));
+  var combined = combiner.obj([
+    gulp.src(config.src),
+        sourcemaps.init(),
+        sass().on('error', sass.logError),
+        sass({outputStyle: 'compressed'}),
+        sourcemaps.write('./'),
+        gulp.dest(config.dest)
+  ]);
+
+  combined.on('error', console.error.bind(console));
+
+  return combined;
 });
 
 gulp.task('sass:watch', function() {
--- gulp/tasks/uglify.js
+++ gulp/tasks/uglify.js
@@ -1,13 +1,23 @@
 'use strict';
 
+// Using stream-combiner2 to catch errors per gulp at:
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/combining-streams-to-handle-errors.md#combining-streams-to-handle-errors
+
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
+    combiner = require('stream-combiner2'),
     config = require('../config').scripts;
 
 gulp.task('uglify', function() {
-  return gulp.src(config.src)
-    .pipe(uglify())
-    .pipe(gulp.dest(config.dest));
+  var combined = combiner.obj([
+    gulp.src(config.src),
+    uglify(),
+    gulp.dest(config.dest)
+  ]);
+
+  combined.on('error', console.error.bind(console));
+
+  return combined;
 });
 
 gulp.task('uglify:watch', function() {
