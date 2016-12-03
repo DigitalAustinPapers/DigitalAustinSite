846da0cd44c73ec8ada546d4794753ea119e0c0b
Ben W. Brumfield <benwbrum@gmail.com>
2016-12-03 09:08:10 -0600

    Word cloud searches now persist other search parameters

--- data/cloud.php
+++ data/cloud.php
@@ -18,6 +18,14 @@ include '../data/query.php';
 
 $database = connectToDB();
 
+
+
+function buildTextLink($text) {
+  $new_params = $_GET;
+  $new_params['query'] = $new_params['query'] . ' ' . $text; 
+  return 'search.php?' . http_build_query($new_params);
+}
+
 //Text Cloud
 $sql = buildWordCloudSearchQuery();
 
@@ -26,7 +34,7 @@ $docData = array();
 $result = mysqli_query($GLOBALS["___mysqli_ston"], $sql) or die($sql . "<br>" . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
 while ($row = mysqli_fetch_assoc($result))
 {
-    $row['link'] = 'search.php?query=' . urlencode($row['text']);
+    $row['link'] = buildTextLink($row['text']);
     array_push($docData, $row);
 }
 array_push($jsonOut, $docData);
@@ -38,7 +46,7 @@ $docData = array();
 $result = mysqli_query($GLOBALS["___mysqli_ston"], $sql) or die($sql . "<br>" . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
 while ($row = mysqli_fetch_assoc($result))
 {
-    $row['link'] = 'search.php?query=' . urlencode($row['text']);
+    $row['link'] = buildTextLink($row['text']);
     array_push($docData, $row);
 }
 array_push($jsonOut, $docData);
@@ -49,7 +57,7 @@ $docData = array();
 $result = mysqli_query($GLOBALS["___mysqli_ston"], $sql) or die($sql . "<br>" . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
 while ($row = mysqli_fetch_assoc($result))
 {
-    $row['link'] = 'search.php?query=' . urlencode($row['text']);
+    $row['link'] = buildTextLink($row['text']);
     array_push($docData, $row);
 }
 array_push($jsonOut, $docData);
